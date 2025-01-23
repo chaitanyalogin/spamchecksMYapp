0f5d661d3c8d428bc8e0709d2cf85aade0064258
chaitanyalogin <chaitanyarocks.kulkarni@gmail.com>
2025-01-23 18:25:27 +0530

    app.py

--- app.py
+++ app.py
@@ -1,56 +1,49 @@
 import nltk
 import os
+import ssl
 
-# Ensure NLTK data is downloaded to a specific path
-nltk.download('punkt', download_dir='/tmp/nltk_data')
-nltk.download('stopwords', download_dir='/tmp/nltk_data')
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
-# Set the NLTK data path
-nltk.data.path.append('/tmp/nltk_data')
+# Download NLTK data
+nltk.download('punkt', quiet=True)
+nltk.download('stopwords', quiet=True)
 
 import streamlit as st
 import pickle
 import string
 from nltk.corpus import stopwords
-import nltk
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
 
-
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
-
     y = []
     for i in text:
         if i.isalnum():
             y.append(i)
-
     text = y[:]
     y.clear()
-
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
-
     text = y[:]
     y.clear()
-
     for i in text:
         y.append(ps.stem(i))
-
     return " ".join(y)
 
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
-
 st.title("Email/SMS Spam Classifier")
-
 input_sms = st.text_area("Enter the message")
-
 if st.button('Predict'):
-
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
     # 2. vectorize
